b1c7f665f20776eda8b86e51ff4cace2405c8c1b
ashfame <mail@ashfame.com>
2025-09-02 23:30:31 +0400

    make linter happy

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const TsconfigPathsPlugin = require( 'tsconfig-paths-webpack-plugin' );
 
 module.exports = {
-  ...defaultConfig,
-  resolve: {
-    ...defaultConfig.resolve,
-    plugins: [
-      ...(defaultConfig.resolve.plugins || []),
-      new TsconfigPathsPlugin(),
-    ],
-  },
+	...defaultConfig,
+	resolve: {
+		...defaultConfig.resolve,
+		plugins: [
+			...( defaultConfig.resolve.plugins || [] ),
+			new TsconfigPathsPlugin(),
+		],
+	},
 };
