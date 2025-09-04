ea2cddda8f8fee1c8b709998154d8038a2b2f8a8
ashfame <mail@ashfame.com>
2025-09-04 21:04:18 +0400

    pass

--- src/CollaborativeEditing.tsx
+++ src/CollaborativeEditing.tsx
@@ -2,7 +2,6 @@ import { useDataManager } from './hooks/useDataManager';
 import { PresenceUI } from './components/PresenceUI';
 import { ReadOnlyUI } from './components/ReadOnlyUI';
 import { useEffect } from '@wordpress/element';
-import { useSelect } from '@wordpress/data';
 import { lockEditor, releaseEditor } from './utils';
 import { useCSSClassManager } from '@/hooks/useCSSClassManager';
 import { useGutenbergEditorControls } from '@/hooks/useGutenbergEditorControls';
@@ -10,12 +9,6 @@ import { useGutenbergEditorControls } from '@/hooks/useGutenbergEditorControls';
 export const CollaborativeEditing = () => {
 	const { collaborationMode, state, syncAwareness } = useDataManager();
 	const { isLockHolder, awareness } = state;
-	const { lockedBlocks } = useSelect(
-		( select ) => ( {
-			lockedBlocks: select( 'gce' ).getLockedBlocks(),
-		} ),
-		[]
-	);
 
 	useCSSClassManager( collaborationMode, awareness, isLockHolder );
 	useGutenbergEditorControls( collaborationMode, isLockHolder );
@@ -48,23 +41,6 @@ export const CollaborativeEditing = () => {
 
 		return release;
 	}, [ isLockHolder, collaborationMode ] );
-
-	useEffect( () => {
-		const editorElement = document.querySelector< HTMLElement >(
-			'.editor-visual-editor'
-		);
-		if ( ! editorElement ) {
-			return;
-		}
-
-		if ( lockedBlocks.length > 0 ) {
-			lockEditor( editorElement );
-		} else {
-			releaseEditor( editorElement );
-		}
-
-		return () => releaseEditor( editorElement );
-	}, [ lockedBlocks ] );
 
 	return (
 		<>
--- src/editor.ts
+++ src/editor.ts
@@ -1,24 +1,49 @@
 import { createHigherOrderComponent } from '@wordpress/compose';
 import { useSelect } from '@wordpress/data';
 import { addFilter } from '@wordpress/hooks';
-import { createElement } from '@wordpress/element';
+import { createElement, useRef, useEffect } from '@wordpress/element';
+import { lockEditor, releaseEditor } from '@/utils';
 
 const withLockedClass = createHigherOrderComponent( ( BlockListBlock ) => {
 	return ( props ) => {
 		const { clientId } = props;
 		const { lockedBlocks } = useSelect(
-			( select ) => ( {
+			( select: any ) => ( {
 				lockedBlocks: select( 'gce' ).getLockedBlocks(),
 			} ),
 			[]
 		);
 
+		const blockRef = useRef< HTMLElement >( null );
+		const isLockedRef = useRef( false );
+
 		let className = props.className || '';
-		if ( lockedBlocks.includes( clientId ) ) {
+		const isLocked = lockedBlocks.includes( clientId );
+		if ( isLocked ) {
 			className = `${ className } gce-locked`;
 		}
 
-		return createElement( BlockListBlock, { ...props, className } );
+		useEffect( () => {
+			const element = blockRef.current;
+			if ( ! element ) {
+				return;
+			}
+
+			if ( isLocked && ! isLockedRef.current ) {
+				// Block became locked - add event listeners
+				lockEditor( element );
+				isLockedRef.current = true;
+			} else if ( ! isLocked && isLockedRef.current ) {
+				// Block became unlocked - remove event listeners (if you implement releaseEditor)
+				releaseEditor( element );
+				isLockedRef.current = false;
+			}
+		}, [ isLocked ] );
+
+		const el = createElement( BlockListBlock, { ...props, className } );
+
+		// Wrap in a div with ref to get DOM access for lockEditor
+		return createElement( 'div', { ref: blockRef }, el );
 	};
 }, 'withLockedClass' );
 
